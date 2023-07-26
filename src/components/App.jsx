4e0f50f62b3fa1421a818e937f5bd35d19861057
Maryna-Lasitsyna <121902242+Maryna-Lasitsyna@users.noreply.github.com>
2023-07-27 02:05:22 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class App extends Component {
+  state = {
+    query: '',
+    images: null,
+    page: 1,
+    error: null,
+    isLoading: false,
+  };
+
+  
+}
+
+// key (required)	str	Your API key: 37266298-f115482873e383a99a6f539ae
+
+export default App;
